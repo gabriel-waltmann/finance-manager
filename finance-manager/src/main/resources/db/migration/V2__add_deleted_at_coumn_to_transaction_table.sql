f972237417ed9197ef6a3f915e725b58e628858a
Gabriel Waltmann <gabrielwaltmann.developer@gmail.com>
2026-06-05 07:39:28 -0300

    refactor: remove java

--- finance-manager/src/main/resources/db/migration/V2__add_deleted_at_coumn_to_transaction_table.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE transactions ADD COLUMN deleted_at TIMESTAMP NULL;
--- finance-manager/src/main/resources/db/migration/V3__add_unique_constraint_on_transactions.sql
+++
@@ -1,2 +0,0 @@
-CREATE UNIQUE INDEX idx_unique_transaction ON transactions(date, title, amount)
-WHERE deleted_at IS NULL;
