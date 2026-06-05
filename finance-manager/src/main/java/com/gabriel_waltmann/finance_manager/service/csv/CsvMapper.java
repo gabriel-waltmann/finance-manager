5a8ba65e83deaa98d42f854e4280363744279711
Gabriel Waltmann <gabrielwaltmann.developer@gmail.com>
2026-06-05 07:39:28 -0300

    refactor: remove java

--- finance-manager/src/main/java/com/gabriel_waltmann/finance_manager/service/csv/CsvMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gabriel_waltmann.finance_manager.service.csv;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CsvMapper<T, K> {
-    T mapTo(K k);
-    K unmapFrom(T t);
-}
