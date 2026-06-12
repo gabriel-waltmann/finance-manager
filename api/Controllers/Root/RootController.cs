faa604927f6e30ef3f48980423105093f3b13db8
Gabriel Waltmann <gabrielwaltmann.developer@gmail.com>
2026-06-12 16:39:23 -0300

    feat: add root route

---
+++ api/Controllers/Root/RootController.cs
@@ -0,0 +1,18 @@
+using api.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers.Root;
+
+[ApiController]
+[Tags("Root")]
+[Route("/")]
+public class RootController : ControllerBase
+{
+  [HttpGet]
+  public async Task<ActionResult<RootResponse>> ExecuteAsync()
+  {
+    var output = new RootResponse { Message = "Finance Manager API running.." };
+
+    return StatusCode(200, output);
+  }
+}
